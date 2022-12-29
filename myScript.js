e233861938959e3cf930f50438c0f5fb464c9d24
vinceye <vinceye.cm@gmail.com>
2022-12-29 10:50:50 -0600

    updated mouseover code and grid reset logic so parent sketchpad div doesn't need to be completely removed

--- myScript.js
+++ myScript.js
@@ -1,10 +1,13 @@
-function generateGrid(size) {
+function initialLoad() {
     const sketchpad = document.createElement('div');
     sketchpad.setAttribute('id','sketchpad');
+    document.querySelector('#container').appendChild(sketchpad);
+}
+
+function generateGrid(size) {
+    const sketchpad = document.querySelector('#sketchpad');
     sketchpad.style['grid-template-columns'] = `repeat(${size}, minmax(10px, 1fr))`;
     sketchpad.style['grid-template-rows'] = `repeat(${size}, minmax(10px, 1fr)`;
-    document.querySelector('#container').appendChild(sketchpad);
-
     for (let x = 1; x <= size; x++){
         for (let y = 1; y <= size; y++) {
             const newSquare = document.createElement('div');
@@ -14,24 +17,27 @@ function generateGrid(size) {
     }
 }
 
-function addListeners() {
-    const tiles = document.querySelectorAll('.gridSquare');
-    tiles.forEach(sketchTile => sketchTile.addEventListener('mouseover', () => sketchTile.style['backgroundColor'] = 'blue'));
+function addMouseover() {
+    document.querySelector('#sketchpad').addEventListener('mouseover', (event) => {
+        const tile = event.target;
+        if (tile.classList.contains('gridSquare')) {
+            tile.style['background-color'] = 'blue';
+        }
+    } )
 }
 
 function resetGrid() {
     const reset = document.querySelector('#reset');
-    console.log('test resetgrid function run');
     reset.addEventListener('click', () => {
-        console.log('test click run');
-        const sketchpad = document.getElementById('sketchpad');
-        sketchpad.remove();
+        const oldTiles = document.querySelectorAll('.gridSquare');
+        oldTiles.forEach(tile => tile.remove());
         const newSize = prompt("Please enter the edge size of your new grid (limit 100)");
         generateGrid(newSize);
-        addListeners();
     });
 }
 
-generateGrid(16);
-addListeners();
+initialLoad();
+addMouseover();
 resetGrid();
+
+generateGrid(16);
