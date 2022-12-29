ad170571ba1f8f87bc82adf264f6c1efde29cd03
vinceye <vinceye.cm@gmail.com>
2022-12-29 10:02:12 -0600

    cleaned up code, moved all style declarations to css file, and removed unnecessary lines

--- myScript.js
+++ myScript.js
@@ -1,24 +1,14 @@
 function generateGrid(size) {
-    currentDiv = document.getElementById('container');
     const sketchpad = document.createElement('div');
     sketchpad.setAttribute('id','sketchpad');
-    sketchpad.style['display'] = 'grid';
     sketchpad.style['grid-template-columns'] = `repeat(${size}, minmax(10px, 1fr))`;
     sketchpad.style['grid-template-rows'] = `repeat(${size}, minmax(10px, 1fr)`;
-    sketchpad.style['border'] = 'solid';
-    sketchpad.style['borderColor'] = 'red';  
-    currentDiv.appendChild(sketchpad);
+    document.querySelector('#container').appendChild(sketchpad);
 
     for (let x = 1; x <= size; x++){
         for (let y = 1; y <= size; y++) {
             const newSquare = document.createElement('div');
             newSquare.classList.add('gridSquare');
-            newSquare.style['gridColumn'] = x;
-            newSquare.style['gridRow'] = y;
-            newSquare.style['border'] = 'dashed';
-            newSquare.style['borderColor'] = 'gray';
-            newSquare.style['aspectRatio'] = 1;
-            //newSquare.style['margin'] = '1px';
             sketchpad.appendChild(newSquare);
         }
     }
