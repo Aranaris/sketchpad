b9ed57052b5081d35456df0f26af5f92dc6f7d2e
vinceye <vinceye.cm@gmail.com>
2023-01-07 21:10:42 -0800

    testing fix to touchmove

--- myScript.js
+++ myScript.js
@@ -5,15 +5,19 @@ function initialLoad() {
 }
 
 function generateGrid(size) {
-    const sketchpad = document.querySelector('#sketchpad');
-    sketchpad.style['grid-template-columns'] = `repeat(${size}, minmax(10px, 1fr))`;
-    sketchpad.style['grid-template-rows'] = `repeat(${size}, minmax(10px, 1fr)`;
-    sketchpad.dataset['size'] = size;
-    for (let x = 1; x <= size; x++){
-        for (let y = 1; y <= size; y++) {
-            const newSquare = document.createElement('div');
-            newSquare.classList.add('gridSquare');
-            sketchpad.appendChild(newSquare);
+    if (size >= 100) {
+        return;
+    } else {
+        const sketchpad = document.querySelector('#sketchpad');
+        sketchpad.style['grid-template-columns'] = `repeat(${size}, minmax(10px, 1fr))`;
+        sketchpad.style['grid-template-rows'] = `repeat(${size}, minmax(10px, 1fr)`;
+        sketchpad.dataset['size'] = size;
+        for (let x = 1; x <= size; x++){
+            for (let y = 1; y <= size; y++) {
+                const newSquare = document.createElement('div');
+                newSquare.classList.add('gridSquare');
+                sketchpad.appendChild(newSquare);
+            }
         }
     }
 }
@@ -33,10 +37,12 @@ function addMouseover() {
         const size = Number(sketchpad.dataset['size']);
         const rect = allTiles[0].getBoundingClientRect();
         for (const touch of event.changedTouches) {
-            const x = Math.floor((touch.clientX - rect.left) / rect.width);
-            const y = Math.floor((touch.clientY - rect.top) / rect.height);
-            const tile = allTiles[y * size + x];
-            tile.style['background-color'] = `rgb(${color})`;
+            if (0 <= touch.clientX <= rect.width && 0 <= touch.clientY <= rect.height) {
+                const x = Math.floor((touch.clientX - rect.left) / rect.width);
+                const y = Math.floor((touch.clientY - rect.top) / rect.height);
+                const tile = allTiles[y * size + x];
+                tile.style['background-color'] = `rgb(${color})`;
+            }
         }
     });
 }
